896ed5f8468ffd190412ea30591cac3864a24b92
Brad Traversy <traversymedia@gmail.com>
2024-10-02 12:28:59 -0400

    Check room availability

--- app/actions/bookRoom.js
+++ app/actions/bookRoom.js
@@ -6,6 +6,7 @@ import { ID } from 'node-appwrite';
 import { redirect } from 'next/navigation';
 import checkAuth from './checkAuth';
 import { revalidatePath } from 'next/cache';
+import checkRoomAvailability from './checkRoomAvailability';
 
 async function bookRoom(previousState, formData) {
   const sessionCookie = cookies().get('appwrite-session');
@@ -30,16 +31,30 @@ async function bookRoom(previousState, formData) {
     const checkInTime = formData.get('check_in_time');
     const checkOutDate = formData.get('check_out_date');
     const checkOutTime = formData.get('check_out_time');
+    const roomId = formData.get('room_id');
 
     // Combine date and time to ISO 8601 format
     const checkInDateTime = `${checkInDate}T${checkInTime}`;
     const checkOutDateTime = `${checkOutDate}T${checkOutTime}`;
 
+    // Check if room is available
+    const isAvailable = await checkRoomAvailability(
+      roomId,
+      checkInDateTime,
+      checkOutDateTime
+    );
+
+    if (!isAvailable) {
+      return {
+        error: 'This room is already booked for the selected time',
+      };
+    }
+
     const bookingData = {
       check_in: checkInDateTime,
       check_out: checkOutDateTime,
       user_id: user.id,
-      room_id: formData.get('room_id'),
+      room_id: roomId,
     };
 
     // Create booking
---
+++ app/actions/checkRoomAvailability.js
@@ -0,0 +1,65 @@
+'use server';
+
+import { createSessionClient } from '@/config/appwrite';
+import { cookies } from 'next/headers';
+import { Query } from 'node-appwrite';
+import { redirect } from 'next/navigation';
+import { DateTime } from 'luxon';
+
+// Convert a date string to a Luxon DateTime object in UTC
+function toUTCDateTime(dateSring) {
+  return DateTime.fromISO(dateSring, { zone: 'utc' }).toUTC();
+}
+
+// Check for overlapping date ranges
+function dateRangesOverlap(checkInA, checkOutA, checkInB, checkOutB) {
+  return checkInA < checkOutB && checkOutA > checkInB;
+}
+
+async function checkRoomAvailability(roomId, checkIn, checkOut) {
+  const sessionCookie = cookies().get('appwrite-session');
+  if (!sessionCookie) {
+    redirect('/login');
+  }
+
+  try {
+    const { databases } = await createSessionClient(sessionCookie.value);
+
+    const checkInDateTime = toUTCDateTime(checkIn);
+    const checkOutDateTime = toUTCDateTime(checkOut);
+
+    // Fetch all bookings for a given room
+    const { documents: bookings } = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_BOOKINGS,
+      [Query.equal('room_id', roomId)]
+    );
+
+    // Loop over bookingsa and check for overlaps
+    for (const booking of bookings) {
+      const bookingCheckInDateTime = toUTCDateTime(booking.check_in);
+      const bookingCheckOutDateTime = toUTCDateTime(booking.check_out);
+
+      if (
+        dateRangesOverlap(
+          checkInDateTime,
+          checkOutDateTime,
+          bookingCheckInDateTime,
+          bookingCheckOutDateTime
+        )
+      ) {
+        return false; // Overlap found, do not book
+      }
+    }
+
+    // No overlap found, continue to book
+    return true;
+  } catch (error) {
+    console.log('Failed to check availability', error);
+    return {
+      error: 'Failed to check availability',
+    };
+  }
+}
+
+export default checkRoomAvailability;
